bb973e11d9e9db57fc7ba7b2218b7f3f35918266
Nicolas cacace <you@example.com>
2025-05-15 20:13:53 -0300

    Cintas png

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -1,8 +1,22 @@
 extends TileMapLayer
 
+var rotacion = 0
+
 
 func _physics_process(delta: float) -> void:
 	
+	if Input.is_action_just_pressed("ruedaMouseArriba"):
+		if rotacion >= 3:
+			rotacion = 0
+		else:
+			rotacion += 1
+	
+	if Input.is_action_just_pressed("ruedaMouseAbajo"):
+		if rotacion <= 0:
+			rotacion = 3
+		else:
+			rotacion -= 1
+	
 	if Input.is_action_pressed("ClickIzdo"):
 		print(get_global_mouse_position() / 32)
-		set_cell(get_global_mouse_position() / 32, 0, Vector2i(1, 1))
+		set_cell(get_global_mouse_position() / 32, 1, Vector2i(rotacion, 0))
